243fff4323db9dce7a5ccdefacc3b3ae1752fc59
imamityaadav <89591490+imamityaadav@users.noreply.github.com>
2024-03-19 17:56:59 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,43 +20,7 @@ variable "region" {
 
 variable "cidr_block" {
   type        = string
-  description = "CIDR block for the VPC"variable "name" {
-  default     = "test-vpc"
-  type        = string
-  description = "Name of the VPC"
-}
-
-variable "project" {
-  default     = "testing"
-  type        = string
-  description = "Name of project this VPC is meant to house"
-}
-
-variable "environment" {
-  default     = "dev"
-  type        = string
-  description = "Name of environment this VPC is targeting"
-}
-
-variable "region" {
-  default     = "ap-south-1"
-  type        = string
-  description = "Region of the VPC"
-}
-
-variable "cidr_block" {
-  default     = "10.0.0.0/16"
-  type        = string
   description = "CIDR block for the VPC"
-}
-
-variable "availability_zone_one" {
-  default     = "ap-south-1a"
-  type        = string
-  description = "List of availability zone 1"
-}
-
-
 }
 
 variable "availability_zone_one" {
